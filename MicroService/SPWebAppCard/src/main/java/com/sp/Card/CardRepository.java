077346249e34ac74303243df880d16d1b94c6772
Jagen <78451338+Jagen22@users.noreply.github.com>
2022-05-24 17:58:35 +0200

    Delete MicroService/SPWebAppCard directory

--- MicroService/SPWebAppCard/src/main/java/com/sp/Card/CardRepository.java
+++
@@ -1,8 +0,0 @@
-package com.sp.Card;
-import java.util.List;
-import org.springframework.data.repository.CrudRepository;
-
-public interface CardRepository extends CrudRepository<Card, Integer> {
-
-	public List<Card> findByPrice(String price);
-}
